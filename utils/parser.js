a76107d4c0652499c314709b414be4308d3739bc
mdsumonfreeluncerbdapps1-prog <mdsumonfreeluncerbdapps1@gmail.com>
2026-04-18 05:59:12 +0600

    Add parseMatchTitle function to parse match titles

--- utils/parser.js
+++ utils/parser.js
@@ -1,1 +1,25 @@
+function parseMatchTitle(match){
 
+ const name = match.match_name || "";
+
+ const matchType =
+ name.match(/\d+(st|nd|rd|th)\s(Test|ODI|T20I)/i);
+
+ const type = matchType ? matchType[0] : "Match";
+
+ const teams =
+ name.match(/([A-Z]{2,4})\svs\s([A-Z]{2,4})/i);
+
+ if(teams){
+
+  return `${type} ${teams[1]} vs ${teams[2]}`;
+
+ }
+
+ return type;
+
+}
+
+module.exports = {
+ parseMatchTitle
+};
