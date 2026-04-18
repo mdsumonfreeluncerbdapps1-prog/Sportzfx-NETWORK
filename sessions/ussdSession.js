d27eb2ae919390b3b5ec8d25e99687db03bb54dd
mdsumonfreeluncerbdapps1-prog <mdsumonfreeluncerbdapps1@gmail.com>
2026-04-18 06:00:13 +0600

    Implement session management for USSD

--- sessions/ussdSession.js
+++ sessions/ussdSession.js
@@ -1,1 +1,20 @@
+let sessions = {};
 
+function getSession(sessionId){
+
+ if(!sessions[sessionId]){
+
+  sessions[sessionId] = {
+   menu:"main",
+   page:0
+  };
+
+ }
+
+ return sessions[sessionId];
+
+}
+
+module.exports = {
+ getSession
+};
