5a82e94c85fe906af94f1ffbad55cd4e321f1a95
mdsumonfreeluncerbdapps1-prog <mdsumonfreeluncerbdapps1@gmail.com>
2026-04-21 19:16:46 +0600

    Refactor parseMatchTitle function for team abbreviations
    git add .
    git commit -m "short match title format"
    git push

--- utils/parser.js
+++ utils/parser.js
@@ -2,30 +2,25 @@ function parseMatchTitle(match){
 
  const name = match.match_name || "";
 
- // detect match type
- const matchType =
- name.match(/\d+(st|nd|rd|th)\s(Test|ODI|T20I)/i);
-
- let type = matchType ? matchType[0] : "";
-
  // detect teams
  const teams =
  name.match(/([A-Za-z]{2,})\s+vs\s+([A-Za-z]{2,})/i);
 
  if(teams){
 
-  const team1 = teams[1].toUpperCase();
-  const team2 = teams[2].toUpperCase();
+  const team1 = teams[1].substring(0,3).toUpperCase();
+  const team2 = teams[2].substring(0,3).toUpperCase();
 
-  if(type){
-   return `${type} ${team1} vs ${team2}`;
-  }
-
-  return `${team1} vs ${team2}`;
+  return `${team1} v ${team2}`;
  }
 
- if(type){
-  return type;
+ // fallback
+ if(match.team1 && match.team2){
+
+  const team1 = match.team1.substring(0,3).toUpperCase();
+  const team2 = match.team2.substring(0,3).toUpperCase();
+
+  return `${team1} v ${team2}`;
  }
 
  return "Match";
