18485481aa50231b5b63388c7add8a4ebc47a9e3
mdsumonfreeluncerbdapps1-prog <mdsumonfreeluncerbdapps1@gmail.com>
2026-04-23 02:00:14 +0600

    Add USSD routes for health check and receive endpoint

--- src/routes/ussdRoutes.js
+++ src/routes/ussdRoutes.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/ussdController");
 
+// health check
+router.get("/", (req, res) => {
+  res.send("USSD Server Running");
+});
+
+// main ussd
+router.post("/ussd/receive", controller.handleUSSD);
+
+module.exports = router;
