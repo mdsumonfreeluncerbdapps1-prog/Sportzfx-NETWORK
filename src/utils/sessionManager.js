f94cc8e811bb9f05ead66a87d6b0270a563aa371
mdsumonfreeluncerbdapps1-prog <mdsumonfreeluncerbdapps1@gmail.com>
2026-04-23 02:11:18 +0600

    Implement session management functions

--- src/utils/sessionManager.js
+++ src/utils/sessionManager.js
@@ -1,1 +1,13 @@
+const sessions = {};
 
+exports.set = (id, data) => {
+  sessions[id] = data;
+};
+
+exports.get = (id) => {
+  return sessions[id] || {};
+};
+
+exports.clear = (id) => {
+  delete sessions[id];
+};
