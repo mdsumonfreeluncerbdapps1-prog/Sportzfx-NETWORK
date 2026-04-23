a12ae53011fe48765fd1a9f3a4ddb06c4c13ff2e
mdsumonfreeluncerbdapps1-prog <mdsumonfreeluncerbdapps1@gmail.com>
2026-04-24 03:19:01 +0600

    Add functions to fetch live, upcoming, and recent matches

--- src/services/cricketService.js
+++ src/services/cricketService.js
@@ -1,1 +1,16 @@
+const axios = require("axios");
 
+exports.getLive = async () => {
+  const res = await axios.get(process.env.LIVE_API);
+  return res.data.split("\n");
+};
+
+exports.getUpcoming = async () => {
+  const res = await axios.get(process.env.UPCOMING_API);
+  return res.data.split("\n");
+};
+
+exports.getRecent = async () => {
+  const res = await axios.get(process.env.RECENT_API);
+  return res.data.split("\n");
+};
